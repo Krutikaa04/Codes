7b2eec8c2b98c6f8a5cdeb1a920cf10895e81670
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-03-17 11:52:05 +0530

    Add files via upload

---
+++ c/loops.c
@@ -0,0 +1,26 @@
+//loops
+types -- 
+for, while, do while
+
+//for loop
+for ( int i = 0; i < count; i++)
+{
+    /* code */
+}
+
+in for loop --- initialization, condition, increment/decrement
+++i/--i --- pre-increment/pre-decrement operators
+i++/i-- ---post-increment/post-decrement operators
+
+//while loop
+while (/* condition */)
+{
+    /* code */
+}
+
+//do while loop
+do
+{
+    /* code */
+} while (/* condition */);
+
