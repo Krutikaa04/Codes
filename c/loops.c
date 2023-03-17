8847fa309a190c4b8f25f28083051db788a4a9ce
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-03-17 11:54:28 +0530

    Update loops.c

--- c/loops.c
+++ c/loops.c
@@ -24,3 +24,12 @@ do
     /* code */
 } while (/* condition */);
 
+//example
+print numbers from 1 to 10
+    #include<stdio.h>
+    int main() {
+    for (int i = 0; i <=10; i++) {
+        printf(i);
+    }
+    return 0;
+}
