66d2a08e67aa1d6602368b00248103e5a159b223
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-04-05 18:51:39 +0530

    Update Intro.java

--- Java/Intro.java
+++ Java/Intro.java
@@ -1,7 +1,26 @@
 //hello world in java
 
+import java.io.*;
+import java.lang.*;
+
 class Intro {
   public static void main(String[] args) {
     System.out.println("Hello world");
+  }
+}
+
+//data types --> int, float, char, string, array, etc
+
+
+//taking inputs in java
+taking inputs in java requires Scanner class
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Number {
+  public static void add(){
+    
   }
 }
