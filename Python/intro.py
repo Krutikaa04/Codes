245eb134e000bb57043b638893c9ab54b7ba28f5
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-04-09 12:45:03 +0530

    Create intro.py

---
+++ Python/intro.py
@@ -0,0 +1,21 @@
+#printing hello world in python    --->this is how comments are made in python
+print("Hello World!")
+
+#data types and variables
+#Python does not require declaration of type like integer, float, string, etc.
+print(1+1)       
+print(10%2)
+print(30 ** 2)
+
+#You can print type of the variable as follows :-
+var1 = 12
+var2 = "Krutika"
+var3 = 45.63
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+
+#printing length of string
+print(len(var2))
