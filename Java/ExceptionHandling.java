69663649a6eef2446f647d0cd5c2cef22c6d993e
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-04-14 19:58:57 +0530

    Add files via upload

---
+++ Java/ExceptionHandling.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class ExceptionHandling {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter first value:");
+            int x = sc.nextInt();
+            System.out.println("Enter second value:");
+            int y = sc.nextInt();
+            if(y == 0) {
+                throw new ArithmeticException ("Division by 0 is not possible");
+            }
+            int quotient = x / y;
+            System.out.println("Answer is:"+quotient);
+        }
+        catch (Exception e) {
+            System.out.println("An exception ocurred "+e);
+        }
+        finally {
+            System.out.println("Finally block has been executed");
+        }
+    }
+}
