855c22eb7f5d0d963892fceb5275a0ef4951537e
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-04-15 16:49:32 +0530

    Update Multithreading.java

--- Java/Multithreading.java
+++ Java/Multithreading.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-class First extends Thread {
-    public void run() {
+class First extends Thread {             //Thread is a predefined class
+    public void run() {       
         int i;
         for(i=1; i<=5; i++) {
             System.out.println("First Thread: "+i);
             try {
-                sleep(3000);
+                sleep(3000);            //there will be a gap of 3 secs during execution of loop
             }catch(Exception e) {
                 System.out.println(e);
             }
@@ -32,7 +32,7 @@ class Multithreading {
     public static void main(String[] args) {
         First t1 = new First();
         Second t2 = new Second();
-        t1.start();
+        t1.start();          //start is a predefined function
         t2.start();
     }
 }
