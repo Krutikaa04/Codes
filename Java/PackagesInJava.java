a4e12aceb992dfb8cd83485d10996fd4e3157041
Krutika Wagh <123003992+Krutikaa04@users.noreply.github.com>
2023-05-06 09:06:47 +0530

    Add files via upload

---
+++ Java/PackagesInJava.java
@@ -0,0 +1,8 @@
+import MyPackage.MyPackage;
+class PackagesInJava {
+    public static void main(String[] args) {
+        MyPackage obj = new MyPackage();
+        obj.function();
+        System.out.println("Package used successfully!");
+    }
+}
